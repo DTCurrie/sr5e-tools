a15c48cce805249b30d4e8ebd2e94aafcfe7fdbe
devin <devintcurrie@gmail.com>
2020-05-05 17:20:55 -0400

    Updating precommit hook

--- apps/run-generator/src/test.ts
+++ apps/run-generator/src/test.ts
@@ -7,18 +7,6 @@ console.log("precommit test");
 console.log("precommit test");
 console.log("precommit test");
 
-
-
-
-
-
-
-
-
-
-
-
-
 console.log("precommit test");
 console.log("precommit test");
 console.log("precommit test");
